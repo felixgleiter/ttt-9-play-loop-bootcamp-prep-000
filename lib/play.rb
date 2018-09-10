396a5c0acdb8d9e11b9515b21ae9adcad4906807
Felix Godfree <felixfgodfree@gmail.com>
2018-09-10 11:22:54 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,7 +36,10 @@ def turn(board)
 end
 
 def play(board)
-  turn(board)
+  turn_counter = 1
+  while turn_counter <= 9 
+    turn(board)
+  end
   if (position_taken?(board, 1) && board[0] == board[1] && board[0] == board[2]) || (position_taken?(board, 4) && board[3] == board[4] && board[3] == board[5]) || (position_taken?(board, 7) && board[6] == board[7] && board[6] == board[8]) || (position_taken?(board, 1) && board[0] == board[3] && board[0] == board[6]) || (position_taken?(board, 2) && board[1] == board[4] && board[1] == board[7]) || (position_taken?(board, 3) && board[2] == board[5] && board[2] == board[8]) || (position_taken?(board, 1) && board[0] == board[4] && board[0] == board[8]) || (position_taken?(board, 3) && board[2] == board[4] && board[2] == board[6])
     puts "Congratulations!"
   else
